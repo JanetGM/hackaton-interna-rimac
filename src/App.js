c2455f0b6362bcbe0088ebff4404c3b56ea86c3c
AlexisPescoran <alexisfer.18@gmail.com>
2019-05-09 10:48:32 -0500

    creación de componente questions

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Comparative from '../src/components/Comparative';
+import Questions from '../src/components/Questions'
 
 function App() {
   return (
     <div className="App">
      <Comparative/>
+     <Questions />
     </div>
   );
 }
---
+++ src/components/Questions.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Questions = () => {
+    return (
+        <label>just, checking</label>
+
+    );
+}
+export default Questions;
