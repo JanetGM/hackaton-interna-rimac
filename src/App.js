20903248c89c06761b2270cee3b4d22dc72fa270
yuriquezada <yuri_quezada@hotmail.com>
2019-05-09 13:22:42 -0500

    Muestra header html y css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import Comparative from '../src/components/Comparative';
+import Header from '../src/components/header';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
      <Comparative/>
     </div>
   );
