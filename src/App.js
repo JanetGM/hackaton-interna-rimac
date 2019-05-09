85e80dd167234f71940702aa75d4051fc0c629fb
JanetGM <antaly.146@gmail.com>
2019-05-09 10:06:32 -0500

    fixed error node-modules y agregando componente Comparative

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Comparative from '../src/components/Comparative';
+
+function App() {
+  return (
+    <div className="App">
+     <Comparative/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Comparative.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+ const Comparative = () => {
+ return(
+      <div className="container-fluid">
+      <div className="row">
+        <div className="header-tab col-7">
+         <span className="offset-5">Plan Edificación</span>
+         <span className="offset-1">Plan Edificación y Contenido</span>
+        </div>
+      </div>
+    <div className="row">
+    <div className="content-tab mr-5 col-1">
+        <span className="mr-5">Asegura la estructura de tu hogar</span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+    </div>
+      
+    );
+ }
+ export default Comparative;
