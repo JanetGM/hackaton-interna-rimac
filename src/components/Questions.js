1c7481e6bc76ebb8f54c35153da0f739a637984a
AlexisPescoran <alexisfer.18@gmail.com>
2019-05-09 11:12:59 -0500

    estructura inicial del compomente Questions

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -2,8 +2,21 @@ import React from 'react'
 
 const Questions = () => {
     return (
-        <label>just, checking</label>
+        <section>
+            <hr></hr>
+            <div>
+                <h3>Hola, soy Peter. Te guiaré a cotizar un precio increíble en segundos.
+                    ¿Estás listo?</h3>
+                <input type="text" placeholder="nombre"></input>
+                <input type="text" placeholder="apellido"></input>
+            </div>
+            <div>
+                <button>Hagámoslo</button>
+            </div>
+            
 
+        </section>
+        
     );
 }
 export default Questions;
