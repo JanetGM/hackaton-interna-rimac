fbc1d94de49f016e5da2f34a0e499d5b713054f8
AlexisPescoran <alexisfer.18@gmail.com>
2019-05-10 10:12:52 -0500

    Updates

---
+++ precache-manifest.6368e5bc74ae1ba0e8c3b2f5676676bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18208675e7beaf1ae4f8751a03cec619",
+    "url": "/hackaton-interna-rimac/index.html"
+  },
+  {
+    "revision": "e173d0945b832ceb0f84",
+    "url": "/hackaton-interna-rimac/static/css/main.3420d900.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/hackaton-interna-rimac/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "e173d0945b832ceb0f84",
+    "url": "/hackaton-interna-rimac/static/js/main.affe336b.chunk.js"
+  },
+  {
+    "revision": "60f03d47686521fb441d",
+    "url": "/hackaton-interna-rimac/static/js/runtime~main.0c2df896.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/hackaton-interna-rimac/precache-manifest.6368e5bc74ae1ba0e8c3b2f5676676bc.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hackaton-interna-rimac/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
