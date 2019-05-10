b63d37688580b1e3222fc319950a56401027444d
yuriquezada <yuri_quezada@hotmail.com>
2019-05-09 23:53:21 -0500

    Muestra el archivo Style.scss, y App.js llama al archivo generado por Sass: Style.css

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import './App.css';
+import './Style.css';
 import Comparative from '../src/components/Comparative';
 import NameQuestion from './components/NameQuestion'
 import AddressQuestion from './components/AddressQuestion'
 import ProgressBar from './components/ProgressBar'
 import Header from '../src/components/header'
 import OwnerQuestion from './components/OwnerQuestion'
+import Banner from './components/banner';
 
 function App() {
   return (
     <div className="App">
      <Header/>
+     <Banner/>
      <Comparative/>
      <ProgressBar />
      <NameQuestion />
---
+++ src/components/banner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Banner = () => {
+  return (
+    <div>
+      <section className="rimac-banner-hogar">
+        Soy el bannercito
+      </section>
+
+    </div>
+  );
+}
+export default Banner;
